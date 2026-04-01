501c1e1d6b1c21a2be706db3a06953e127b7a649
jesusnDev <jesusndev@gmail.com>
2026-04-01 11:25:54 -0600

    test: disable context load test to avoid db connection in C

--- src/test/java/com/jesusn/booking/inventory_service/InventoryServiceApplicationTests.java
+++ src/test/java/com/jesusn/booking/inventory_service/InventoryServiceApplicationTests.java
@@ -1,10 +1,12 @@
 package com.jesusn.booking.inventory_service;
 
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class InventoryServiceApplicationTests {
 
 	@Test
